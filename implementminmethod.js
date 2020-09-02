8bbdb9709eda2d1f4d62d88bd63bf9dd247e53e1
lindsay902 <65096921+lindsay902@users.noreply.github.com>
2020-09-01 19:57:11 -0500

    Update implementminmethod.js

--- implementminmethod.js
+++ implementminmethod.js
@@ -4,7 +4,6 @@ const ages = [14, 29, 33, 22, 8, 64];
 const minMethod = (array) => {
     let minArray = [];
     minArray.push(array[0]);
-    //console.log(minArray);
     for (let i=0; i < array.length; i++) {
         for (let j=0; j < array.length; j++) {
             if (array[j] < minArray) {
